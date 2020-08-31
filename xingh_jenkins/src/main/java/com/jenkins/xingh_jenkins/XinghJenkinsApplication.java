63a9cd7ddefde5cf99a4c4582386cdee9911a0e9
xingh <305563004@qq.com>
2020-08-31 09:59:43 +0800

    xingh

---
+++ xingh_jenkins/src/main/java/com/jenkins/xingh_jenkins/XinghJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.xingh_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinghJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinghJenkinsApplication.class, args);
+    }
+
+}
---
+++ xingh_jenkins/src/test/java/com/jenkins/xingh_jenkins/XinghJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.xingh_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinghJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
