63d5fe43eb1a92b3322061bc495d760578111fd0
xingh <305563004@qq.com>
2020-09-01 09:44:42 +0800

    xingh

--- xingh_jenkins/src/main/java/com/jenkins/xingh_jenkins/controller/TestController.java
+++ xingh_jenkins/src/main/java/com/jenkins/xingh_jenkins/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "jenkins!!!";
+        return "jenkins!!!修改了东西的";
     }
 
     @GetMapping("test2")
